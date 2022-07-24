bd46114910ac87b992f47247a953208b1ad16727
Татьяна Овчинникова <tanja-ovc@yandex.ru>
2022-07-24 23:55:36 +0300

    a variable renamed properly

--- yacut/utils.py
+++ yacut/utils.py
@@ -5,5 +5,5 @@ import string
 def get_unique_short_id():
     required_length = 6
     possible_symbols = string.ascii_letters + string.digits
-    id = random.choices(possible_symbols, k=required_length)
-    return ''.join(id)
+    short_id = random.choices(possible_symbols, k=required_length)
+    return ''.join(short_id)
